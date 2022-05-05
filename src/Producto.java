43ac1b88be12c78a4c5da15ba218c1182f17b71b
Lilly <lilimarrerocaparroso@gmail.com>
2022-05-05 10:04:45 +0100

    Feature: clase Producto  creada

---
+++ src/Producto.java
@@ -0,0 +1,34 @@
+public class Producto {
+
+    public String nombre;
+    public float precio;
+
+    public Producto(String nombre, float precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+}
